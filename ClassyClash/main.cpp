b5693b8951f7719256a207a75391ebad53fb16ed
Alex <barankaslon@hotmail.com>
2021-08-17 15:03:19 -0300

    added collision rectangles and collision check

--- ClassyClash/main.cpp
+++ ClassyClash/main.cpp
@@ -20,7 +20,10 @@ int main()
 
     Character knight{windowWidth, windowHeight};
 
-    Prop rock{Vector2{0.f, 0.f}, LoadTexture("nature_tileset/Rock.png")};
+    Prop props[2]{
+        Prop{Vector2{600.f, 300.f}, LoadTexture("nature_tileset/Rock.png")},
+        Prop{Vector2{400.f, 500.f}, LoadTexture("nature_tileset/Log.png")}
+    };
 
     SetTargetFPS(60);
 
@@ -34,7 +37,11 @@ int main()
         // draw the map
         DrawTextureEx(map, mapPos, 0.0, mapScale, WHITE);
 
-        rock.Render(knight.getWorldPos());
+        // Draw the props
+        for(auto prop : props)
+        {
+            prop.Render(knight.getWorldPos());
+        }
         
         knight.tick(GetFrameTime());
         //check map bounds
@@ -45,6 +52,14 @@ int main()
         )
         {
             knight.undoMovement();
+        }
+        //check prop collisions
+        for (auto prop : props)
+        {
+            if (CheckCollisionRecs(prop.getCollisionRec(knight.getWorldPos()), knight.getCollisionRec()))
+            {
+                knight.undoMovement();
+            }
         }
 
         EndDrawing();
